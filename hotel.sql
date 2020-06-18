54026309dba8bcde60469977d38b5baa9a302a0f
mohamedsobhi14 <67109987+mohamedsobhi14@users.noreply.github.com>
2020-06-18 10:12:27 -0700

    Update hotel.sql

--- hotel.sql
+++ hotel.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS Hotel;
+
 CREATE DATABASE Hotel ;
 use Hotel;
 
@@ -23,15 +25,7 @@ CREATE TABLE rooms (
   
 );
 
-CREATE TABLE RoomReservation(
-	Number INT,
-    Rasnumber INT,
-    PRIMARY KEY pk_RoomReservation (Number,Rasnumber),
-    FOREIGN KEY fk_RoomReservation_Room (Number)
-		REFERENCES rooms (Number),
-	FOREIGN KEY fk_RoomReservation_Reservation (Rasnumber)
-		REFERENCES reservations (Rasnumber)
-);
+
 
 CREATE TABLE reservations (
     `Rasnumber`         INT PRIMARY KEY,
@@ -51,3 +45,50 @@ CREATE TABLE GuestReservation (
 );
 
 
+CREATE TABLE RoomReservation(
+	Number INT,
+    Rasnumber INT,
+    PRIMARY KEY pk_RoomReservation (Number,Rasnumber),
+    FOREIGN KEY fk_RoomReservation_Room (Number)
+		REFERENCES rooms (Number),
+	FOREIGN KEY fk_RoomReservation_Reservation (Rasnumber)
+		REFERENCES reservations (Rasnumber)
+);
+
+INSERT INTO guests (`id, ` Fname, `Lname, `Phone, `Address, `Email`, Passwords`) VALUES (null, "Mohamed", "Ahmed","(+20) 01009033357","5 street alhoaze","mohamed900@gmail.com","mohahed2020")
+INSERT INTO guests (`id, ` Fname, `Lname, `Phone, `Address, `Email`, Passwords`) VALUES (null, "Maher", "Ahmed","(+20) 01128637757","Tanta","maher_1080@gmail.com","AHMED##12")
+INSERT INTO guests (`id, ` Fname, `Lname, `Phone, `Address, `Email`, Passwords`) VALUES (null, "Mahmoud", "Hassen","(+20) 01225498922","Alex","mahmoudhassen289@gmail.com","passwords2020")
+INSERT INTO Rooms (`Number, ` level`, `style`, `view`, `beds`) VALUES ("512", "high", "single", "the sea", "only bed")
+INSERT INTO Rooms (`Number, ` level`, `style`, `view`, `beds`) VALUES ("444", "high", "double", "the sea", "2 beds")
+INSERT INTO Reservations (`Resnumber`, `Check in`, `checkout`) VALUES ("210","Monday 20/10/2019","Friday 25/10/2019")
+
+UPDATE Guests  
+SET          Fname = ‘Youssef ‘, Lname= `Ali`, Email=`youssefali231@gmail.com`                                                        
+WHERE     id=’200`;
+
+UPDATE Guests  
+SET	Fname = ‘Nabil ‘, Lname=`Ahmed`, Email="nabilahmed556@gmail.com"	                                                    
+WHERE		id=’210`;
+UPDATE Guests
+SET	Fname = ‘Mohamed ‘, Lname=`Ali`, Email="mohamedali56@gmail.com"
+WHERE		id=’212`;
+UPDATE Guests
+SET	Fname = ‘Khalid ‘, Lname=`Mohamed`, Email="khalidmohamed500@gmail.com"                      WHERE		id=’216`;
+UPDATE Guests
+SET	Fname = ‘Mustafa', Lname=`Mahmoud', Email="mustafamahmoud040@gmail.com"
+WHERE	id=’220`;
+
+DELETE FROM Guests 
+WHERE Fname=’Ahmed ‘; 
+
+DELETE FROM Guests 
+WHERE   Fname=’Mohamed ‘;
+
+DELETE FROM Guests 
+WHERE	 Lname=’Ali‘;
+
+DELETE FROM Guests 
+WHERE	 Lname='Mahmoud‘;
+
+DELETE FROM Rooms
+WHERE	Level='low';
